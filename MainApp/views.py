5abb53eb52a833f27354de72e49de812c3f51e10
shareecheck <katvartes@gmal.com>
2023-09-07 15:22:17 +0300

    Done M5: part2

--- MainApp/views.py
+++ MainApp/views.py
@@ -27,9 +27,37 @@ def add_snippet_page(request):
         return render(request,'pages/add_snippet.html',{'form': form})
     
 def delete_snippet(request, snippet_id):
-    snippet = Snippet.objects.get(id=snippet_id)
-    snippet.delete()
-    return redirect("list")
+    try:
+        snippet = Snippet.objects.get(id=snippet_id)
+    except ObjectDoesNotExist:
+        raise Http404
+    else:
+        snippet.delete()
+        return redirect("list")
+    
+    
+def edit_snippet(request, snippet_id):
+    try:
+        snippet = Snippet.objects.get(id=snippet_id)
+    except ObjectDoesNotExist:
+        raise Http404
+    #Хотим получить страницу сниппета для изменения полей
+    if request.method == 'GET':
+        context = {'pagename': 'Редактирование сниппета',
+                   'snippet': snippet,
+                   'type': 'edit'}
+        return render(request, 'pages/snippet_info.html', context)
+    
+    #Хотим сохранить измененный сниппет
+    if request.method == 'POST':
+        data_form = request.POST
+        snippet.name = data_form['name']
+        snippet.lang = data_form['lang']
+        snippet.creation_date = data_form['creation_date']
+        snippet.code = data_form['code']
+        snippet.save()
+        return redirect("list")
+
 
 
 def snippets_page(request):
@@ -44,7 +72,8 @@ def snippet_info_page(request, snippet_id):
     try:
         snippet = Snippet.objects.get(id=snippet_id)
         context = {'pagename': 'Информация о сниппете',
-                   'snippet': snippet}
+                   'snippet': snippet,
+                   'type': 'view'}
         return render(request, 'pages/snippet_info.html', context)
     except ObjectDoesNotExist:
         return HttpResponseNotFound(f'Сниппет не найден')
--- Snippets/urls.py
+++ Snippets/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('snippets/add', views.add_snippet_page, name="add"),
     path('snippets/list', views.snippets_page, name="list"),
     path('delete/<int:snippet_id>', views.delete_snippet, name="delete"),
+    path('edit/<int:snippet_id>', views.edit_snippet, name="edit"),
     #path('snippets/create', views.create_snippet, name="create"),
     path('snippet/<int:snippet_id>/', views.snippet_info_page, name="snippet_info")
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
